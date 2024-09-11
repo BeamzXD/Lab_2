b6257a1e5c7742a57207c5dbf4c9b5a07218d59c
BeamzXD <makes.k.s@mail.ru>
2024-09-11 10:30:10 +0300

    Add lab

--- Lab2_ConsoleApp/obj/Debug/net8.0/Lab2_ConsoleApp.AssemblyInfo.cs
+++ Lab2_ConsoleApp/obj/Debug/net8.0/Lab2_ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2_ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21a90d558fd1e6b1b9fc5d4ef077d59a3c9fecf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2_ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2_ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
